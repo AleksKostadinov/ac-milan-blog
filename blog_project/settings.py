b75b95c0ef6224f8c123b8e31a0fd83fd58fb605
AleksKostadinov <kostadinov12@gmail.com>
2022-09-15 20:17:23 +0300

    CSS edit

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-cd12.up.railway.app']
+ALLOWED_HOSTS = ['web-production-cd12.up.railway.app','127.0.0.1']
 
 
 # Application definition
