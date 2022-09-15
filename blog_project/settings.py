5758da48a10e8d7e1f1212be4df62e660f905c44
AleksKostadinov <kostadinov12@gmail.com>
2022-09-15 03:03:42 +0300

    fixing

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-import environ
-env = environ.Env()
-environ.Env.read_env()
+# import environ
+# env = environ.Env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY") 
-# SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = env("SECRET_KEY") 
+SECRET_KEY = os.environ['SECRET_KEY']
 
 
 
