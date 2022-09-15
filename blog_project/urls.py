27abf93c31197c983f55c6a0f8053321df3714cf
A.Kostadinov <108954429+AleksKostadinov@users.noreply.github.com>
2022-09-15 18:03:03 +0300

    Update urls.py

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,4 +1,4 @@
-# from re import template
+from re import template
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
@@ -39,4 +39,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
