8293374dafaae45c07f1adf72c6870fcc3c21a42
A.Kostadinov <108954429+AleksKostadinov@users.noreply.github.com>
2022-11-08 03:53:08 +0200

    Update settings.py
    Solved issue for PostgreSQL after removed deployment in Railway.

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # ALLOWED_HOSTS = ['127.0.0.1']
 ALLOWED_HOSTS = os.environ.get('SERVERNAMES').split(' ')
 
